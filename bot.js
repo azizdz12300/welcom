b4e95bb624bdf6c226ea1b0a8caedcbc034d327e
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-03-28 15:49:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("guildMemberAdd", member => {
-  client.channels.find('id', '558783299024060419').send(`**welcome to Venus : [ ${member} ]**`)
+  client.channels.find('id', '558783299024060419').send(`**Welcome To Venus Server:yellow_heart: : [ ${member} ]**`)
 });
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
